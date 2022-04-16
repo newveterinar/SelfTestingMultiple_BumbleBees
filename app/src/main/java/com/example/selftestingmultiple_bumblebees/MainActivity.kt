6cb63f6772c5786e003c57002ee0d7596b38d8e7
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-04-16 17:35:03 +0300

    Создана ветка Feature

--- app/src/main/java/com/example/selftestingmultiple_bumblebees/MainActivity.kt
+++ app/src/main/java/com/example/selftestingmultiple_bumblebees/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity: AppCompatActivity() {
 
     // Создана ветка Develop
+    // Создана ветка Feature
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
