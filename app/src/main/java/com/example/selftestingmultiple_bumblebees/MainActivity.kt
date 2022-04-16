10dcd949e75708c1e6743443cce5dabd3bbed7d1
AVKhakhalin <78661461+AVKhakhalin@users.noreply.github.com>
2022-04-16 17:56:25 +0300

    feature_toast (#2)
    * Начинаем разработку Toast
    
    * Создали элемент Toast (#1)
    
    Закончили создание элемента Toast

--- app/src/main/java/com/example/selftestingmultiple_bumblebees/MainActivity.kt
+++ app/src/main/java/com/example/selftestingmultiple_bumblebees/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.selftestingmultiple_bumblebees
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity: AppCompatActivity() {
 
     // Создана ветка Develop
     // Создана ветка Feature
+    // Начинаем разработку Toast
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Создали элемент Toast
+        Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show()
     }
 }
