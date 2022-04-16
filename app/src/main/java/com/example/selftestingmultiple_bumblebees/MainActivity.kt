d9a24d524423fbcdffc9328c465c12fb5e957525
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-04-16 17:39:28 +0300

    Создали элемент Toast

--- app/src/main/java/com/example/selftestingmultiple_bumblebees/MainActivity.kt
+++ app/src/main/java/com/example/selftestingmultiple_bumblebees/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.selftestingmultiple_bumblebees
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity: AppCompatActivity() {
 
@@ -11,5 +12,8 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Создали элемент Toast
+        Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show()
     }
 }
